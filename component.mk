06aec8ca7529dad7da01c336d33eff4baa58b7a0
kishima <kishima7@gmail.com>
2019-07-21 01:16:55 +0900

    fix compile error

--- component.mk
+++ component.mk
@@ -1,21 +1,8 @@
-
-#COMPONENT_OWNBUILDTARGET := true
-#COMPONENT_OWNCLEANTARGET := true
-
 SDKPATH = $(COMPONENT_PATH)/../arduino/tools/sdk/include
 COREPATH = $(COMPONENT_PATH)/../arduino/cores
 
 COMPONENT_EXTRA_INCLUDES := $(SDKPATH)/esp32 $(SDKPATH)/driver $(SDKPATH)/soc $(SDKPATH)/freertos $(PROJECT_PATH)/build/include $(SDKPATH)/log $(SDKPATH)/ulp $(SDKPATH)/esp_adc_cal $(SDKPATH)/heap $(COREPATH)/esp32 $(COMPONENT_PATH)/../arduino/variants/esp32
 
-COMPONENT_SRCDIRS := $(COMPONENT_PATH)/src
+COMPONENT_SRCDIRS := src
 
 CPPFLAGS += -std=gnu++11 -fno-rtti
-
-#build: 
-	#mkdir -p $(COMPONENT_PATH)/build
-	#cd $(COMPONENT_PATH) && $(MAKE)
-	#cp $(COMPONENT_PATH)/build/libfabgl.a $(COMPONENT_LIBRARY)
-
-#clean: 
-#	#cd $(COMPONENT_PATH) && $(MAKE) clean
-
