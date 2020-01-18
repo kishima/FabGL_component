75bba2854a0adb36f3f1d1458e42844155256b27
kishima <kishima7@gmail.com>
2020-01-18 20:07:19 +0900

    update PATH for v0.8.0

--- component.mk
+++ component.mk
@@ -1,8 +1,8 @@
 SDKPATH = $(COMPONENT_PATH)/../arduino/tools/sdk/include
 COREPATH = $(COMPONENT_PATH)/../arduino/cores
 
-COMPONENT_EXTRA_INCLUDES := $(SDKPATH)/esp32 $(SDKPATH)/driver $(SDKPATH)/soc $(SDKPATH)/freertos $(PROJECT_PATH)/build/include $(SDKPATH)/log $(SDKPATH)/ulp $(SDKPATH)/esp_adc_cal $(SDKPATH)/heap $(COREPATH)/esp32 $(COMPONENT_PATH)/../arduino/variants/esp32
+COMPONENT_EXTRA_INCLUDES := $(SDKPATH)/esp32 $(SDKPATH)/driver $(SDKPATH)/soc $(SDKPATH)/freertos $(PROJECT_PATH)/build/include $(SDKPATH)/log $(SDKPATH)/ulp $(SDKPATH)/esp_adc_cal $(SDKPATH)/heap $(COREPATH)/esp32 $(COMPONENT_PATH)/../arduino/variants/esp32 $(COMPONENT_PATH)/src
 
-COMPONENT_SRCDIRS := src
+COMPONENT_SRCDIRS := src src/comdrivers src/devdrivers src/dispdrivers src/comdrivers
 
 CPPFLAGS += -std=gnu++11 -fno-rtti
